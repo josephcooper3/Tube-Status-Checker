1967ea8757906932bf5696500f782ddceb015dff
Joe Cooper <josephcooper3@gmail.com>
2019-09-15 21:42:20 +0100

    adds color styling for lines

--- src/components/LineDetails.vue
+++ src/components/LineDetails.vue
@@ -1,7 +1,7 @@
 <template>
-  <tr>
-    <td>{{ line.name }} Line</td>
-    <td>{{ line.lineStatuses[0].statusSeverityDescription }}</td>
+  <tr :id="line.id">
+    <td :id="line.id">{{ line.name }} Line</td>
+    <td :id="line.id">{{ line.lineStatuses[0].statusSeverityDescription }}</td>
   </tr>
 </template>
 
@@ -12,6 +12,56 @@ export default {
 }
 </script>
 
-<style>
+<style lang="css">
+  td {
+    color: white;
+  }
+
+  #bakerloo td {
+   background-color: #996633
+  }
+
+  #central td {
+    background-color: #CC3333
+  }
+
+  #circle td {
+    background-color: #FFCC00;
+    color: black
+  }
+
+  #district td {
+    background-color: #006633
+  }
+
+  #hammersmith-city td {
+    background-color: #CC9999;
+    color: black
+  }
+
+  #jubilee td {
+    background-color: #868F98
+  }
+
+  #metropolitan td {
+    background-color: #660066
+  }
+
+  #northern td {
+    background-color: #000000
+  }
+
+  #piccadilly td {
+    background-color: #000099
+  }
+
+  #victoria td {
+    background-color: #0099CC
+  }
+
+  #waterloo-city td {
+    background-color: #66CCCC;
+    color: black
+  }
 
 </style>
