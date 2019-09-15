dcb0d698e9a9f5e108440bc42d2dee48ca7a8f91
Joe Cooper <josephcooper3@gmail.com>
2019-09-15 21:26:30 +0100

    refactors line details to display in a table

--- src/components/LineDetails.vue
+++ src/components/LineDetails.vue
@@ -1,12 +1,14 @@
 <template>
-  <li>{{ line.name }} Line: {{ line.lineStatuses[0].statusSeverityDescription }}</li>
+  <tr>
+    <td>{{ line.name }} Line</td>
+    <td>{{ line.lineStatuses[0].statusSeverityDescription }}</td>
+  </tr>
 </template>
 
 <script>
 export default {
   name: "line-details",
   props: ["line"]
-
 }
 </script>
 
--- src/components/StatusList.vue
+++ src/components/StatusList.vue
@@ -1,9 +1,9 @@
 <template>
 <div>
   <h3>Status Details</h3>
-  <ul>
+  <table width="100%">
     <line-details v-for="line in lines" v-bind:line="line" v-bind:key="line.id"></line-details>
-  </ul>
+  </table>
 </div>
 </template>
 
