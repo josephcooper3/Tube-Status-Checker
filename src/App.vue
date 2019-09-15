7814c68057dc07133b78ff8c2b36154e3ca93eea
Joe Cooper <josephcooper3@gmail.com>
2019-09-15 21:23:25 +0100

    adds ability to filter lines by status

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="app">
     <h1>Tube Line Status Checker</h1>
-    <status-list :lines="statusDetails"></status-list>
+    <label for="filter">Filter Lines: </label>
+    <select id="filter" v-model="filterSelected">
+      <option value="all-lines">Show All Lines</option>
+      <option value="good-lines">Show Only Lines with Good Service</option>
+      <option value="bad-lines">Show Only Disrupted Lines</option>
+    </select>
+    <status-list :lines="filteredLines"></status-list>
     <button v-on:click="fetchStatusDetails()">Refresh Status</button>
     <footer>Powered by TfL Open Data. Contains OS data © Crown copyright and database rights 2016 and Geomni UK Map data © and database rights 2019</footer>
   </div>
@@ -17,17 +23,21 @@ export default {
   data() {
     return {
       statusDetails: [],
+      filterSelected: 'all-lines'
     }
   },
   mounted() {
     this.fetchStatusDetails();
   },
   computed: {
-    goodLines: function () {
-      return this.statusDetails.filter((line) => line.lineStatuses[0].statusSeverityDescription === 'Good Service')
-    },
-    badLines: function () {
-      return this.statusDetails.filter((line) => line.lineStatuses[0].statusSeverityDescription !== 'Good Service')
+    filteredLines: function () {
+      if (this.filterSelected === 'good-lines') {
+        return this.statusDetails.filter((line) => line.lineStatuses[0].statusSeverityDescription === 'Good Service')
+      } else if (this.filterSelected === 'bad-lines') {
+        return this.statusDetails.filter((line) => line.lineStatuses[0].statusSeverityDescription !== 'Good Service')
+      } else {
+        return this.statusDetails
+      }
     }
   },
   components: {
@@ -39,7 +49,7 @@ export default {
       .then(result => result.json())
       .then(statusDetails => this.statusDetails = statusDetails)
       .catch(console.error);      
-    } 
+    },
   }
 }
 </script>
