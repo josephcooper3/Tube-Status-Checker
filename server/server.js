c8b7b378275bd109b8cb3fbd542e3096c76811a4
Joe Cooper <josephcooper3@gmail.com>
2019-11-03 12:13:42 +0000

    app now uses .env variables for api key

--- server/server.js
+++ server/server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const async = require('express-async-await')
 const fetch = require('node-fetch');
-const Key = require('./key');
+
+const dotenv = require('dotenv');
+dotenv.config();
+const appId = process.env.APP_ID;
+const appKey = process.env.APP_KEY;
 
 app.get('/', async function (req, res) {
-  fetch(`https://api.tfl.gov.uk/Line/Mode/tube/Status?detail=true&app_id=${Key.app_id}&app_key=${Key.app_key}`)
+  fetch(`https://api.tfl.gov.uk/Line/Mode/tube/Status?detail=true&app_id=${ appId }&app_key=${ appKey }`)
   .then(result => result.json())
   .then(statusDetails => res.json(statusDetails))
   .catch(console.error);      
