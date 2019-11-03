b11124ccae37d291009297f71a60a3c164035141
Joe Cooper <josephcooper3@gmail.com>
2019-11-03 12:25:54 +0000

    refactors

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-const async = require('express-async-await')
 const fetch = require('node-fetch');
 
 const dotenv = require('dotenv');
 dotenv.config();
 const appId = process.env.APP_ID;
 const appKey = process.env.APP_KEY;
+const port = process.env.PORT;
 
 app.get('/', async function (req, res) {
   fetch(`https://api.tfl.gov.uk/Line/Mode/tube/Status?detail=true&app_id=${ appId }&app_key=${ appKey }`)
@@ -16,5 +16,5 @@ app.get('/', async function (req, res) {
 });
 
 app.listen(3000, function () {
-  console.log('App running on port 3000');
+  console.log(`App running on port ${ port }`);
 });
